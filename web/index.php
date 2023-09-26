09329e3c46cc079091305807760fb06189bbe562
aj <aj@ajs-MacBook-Pro.local>
2023-09-26 08:28:05 +0200

    git init project

---
+++ web/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Index Page</title>
+</head>
+<body>
+    <h1>Welcome to the Index Page</h1>
+    <p>HTML page with links to two other pages:</p>
+
+    <ul>
+        <li><a href="test-1.php">Go to Page 1</a></li>
+        <li><a href="test-2.php">Go to Page 2</a></li>
+    </ul>
+</body>
+</html>
