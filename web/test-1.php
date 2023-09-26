11f62afbe22f70436deb73d594e4bf69a9ee4e78
aj <aj@ajs-MacBook-Pro.local>
2023-09-26 15:35:00 +0200

    adding a bit of style so that forms look a bit better

--- web/test-1.php
+++ web/test-1.php
@@ -2,26 +2,30 @@
 <html>
 <head>
     <title>Page 1</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
-    <h1>Data Entry Form</h1>
+    <header>
+        <h1>Data Entry Form</h1>
+    </header>
 
-    <form action="process_form_test_1.php" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required><br><br>
+    <div class="container">
+        <form action="process_form.php" method="post">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
 
-        <label for="surname">Surname:</label>
-        <input type="text" id="surname" name="surname" required><br><br>
+            <label for="surname">Surname:</label>
+            <input type="text" id="surname" name="surname" required>
 
-        <label for="idNumber">ID Number:</label>
-        <input type="text" id="idNumber" name="idNumber" required pattern="[0-9]{13}" title="ID Number must be 13 digits"><br><br>
+            <label for="idNumber">ID Number:</label>
+            <input type="text" id="idNumber" name="idNumber" required pattern="[0-9]{13}" title="ID Number must be 13 digits">
 
-        <label for="dob">Date of Birth (ddmmyyyy):</label>
-        <input type="text" id="dob" name="dob" required pattern="\d{8}" title="Date of Birth must be in ddmmyyyy format"><br><br>
+            <label for="dob">Date of Birth (ddmmyyyy):</label>
+            <input type="text" id="dob" name="dob" required pattern="\d{8}" title="Date of Birth must be in ddmmyyyy format">
 
-        <button type="submit" name="submit">Submit</button>
-        <button type="reset">Cancel</button>
-    </form>
+            <button type="submit" name="submit">Submit</button>
+            <button type="reset">Cancel</button>
+        </form>
+    </div>
 </body>
 </html>
-
--- web/test-2.php
+++ web/test-2.php
@@ -2,22 +2,27 @@
 <html>
 <head>
     <title>Page 2</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
-    <h1>CSV Generation and Import</h1>
+    <header>
+        <h1>CSV Generation and Import</h1>
+    </header>
 
-    <!-- Form for generating CSV -->
-    <form action="generate_csv.php" method="post">
-        <label for="recordCount">Enter the number of records to generate:</label>
-        <input type="number" id="recordCount" name="recordCount" required>
-        <button type="submit" name="generate">Generate CSV</button>
-    </form>
+    <div class="container">
+        <!-- Form for generating CSV -->
+        <form action="generate_csv.php" method="post">
+            <label for="recordCount">Enter the number of records to generate:</label>
+            <input type="number" id="recordCount" name="recordCount" required>
+            <button type="submit" name="generate">Generate CSV</button>
+        </form>
 
-    <!-- Form for importing CSV into SQLite -->
-    <form action="import_csv.php" method="post" enctype="multipart/form-data">
-        <label for="csvFile">Choose a CSV file to import:</label>
-        <input type="file" id="csvFile" name="csvFile" accept=".csv" required>
-        <button type="submit" name="import">Import CSV</button>
-    </form>
+        <!-- Form for importing CSV into SQLite -->
+        <form action="import_csv.php" method="post" enctype="multipart/form-data">
+            <label for="csvFile">Choose a CSV file to import:</label>
+            <input type="file" id="csvFile" name="csvFile" accept=".csv" required>
+            <button type="submit" name="import">Import CSV</button>
+        </form>
+    </div>
 </body>
 </html>
