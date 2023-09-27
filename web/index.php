a3b0401dc05fc143948a1c1f497d820336deb3e9
aj <aj@ajs-MacBook-Pro.local>
2023-09-27 21:48:56 +0200

    updates and added todo reminders

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 
 <div class="container">
-    <h1 lass="text-center">Welcome to the web</h1>
+    <h1 class="text-center">Welcome to the web</h1>
 </div>
 
 <?php include 'footer.php'; ?>
