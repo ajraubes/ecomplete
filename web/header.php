c123fe4a0593d1aa3d4aa4a6e2be238403ad00bc
aj <aj@ajs-MacBook-Pro.local>
2023-09-27 14:52:50 +0200

    update nginx and images to work

--- web/header.php
+++ web/header.php
@@ -19,7 +19,7 @@
 <body>
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="/"><img src="/images/AJ.svg" alt="Logo" width="75" height="75"></a>
+      <a class="navbar-brand" href="/"><img src="/images/AJ.png" alt="Logo" width="75" height="75"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
