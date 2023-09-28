f2d8042f2fa8c37d527cac43e416622c3ab5fa79
aj <aj@ajs-MacBook-Pro.local>
2023-09-28 21:38:49 +0200

    update forms and struckture clean up

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'; ?>
     <h1 class="text-center">Welcome to My Test Awnsers</h1>
-    <p class="text-center">Hello there! Welcome to my test awnsers. Don't worry; I promise the tests won't bite. In fact, they might even make you smile, just like this developer joke:</p>
+    <p class="text-center">Hello there! Welcome to my test awnsers. This is just an ice breaker:</p>
     <p class="text-center">Why do programmers always mix up Christmas and Halloween? Because Oct 31 == Dec 25!</p>
     <p class="text-center">See you soon!</p>
 <?php include 'footer.php'; ?>
